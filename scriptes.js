f1289463e75386e395cdff9028659944ff7f4298
sanskrutin19 <naiksanskruti03@gmail.com>
2025-03-10 20:57:12 +0530

    more files were added

---
+++ scriptes.js
@@ -0,0 +1,10 @@
+document.getElementById("togglePassword").addEventListener("click", function () {
+    const passwordInput = document.getElementById("password");
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        this.textContent = "Hide";
+    } else {
+        passwordInput.type = "password";
+        this.textContent = "Show";
+    }
+});
